26a70388d522e5db4142c073c2ea275f89492471
Awdhah A N H S A Alazemi <83318@kfh.com>
2024-02-04 16:01:20 +0300

    second task

--- src/main/java/com/coded/SecureBankSystem/controller/CodedController.java
+++ src/main/java/com/coded/SecureBankSystem/controller/CodedController.java
@@ -3,10 +3,14 @@ package com.coded.SecureBankSystem.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 @RestController
 public class CodedController {
+    public ArrayList<Contact> contact = new ArrayList<>();
+
+
     @GetMapping("/sayHi")
     public String sayHi() {
         return "welcome to Coded";
@@ -22,7 +26,30 @@ public class CodedController {
         String name = farewellRequest.getName();
         return "Goodbye, " + name;
     }
+
+    @PostMapping("/addContact")
+    public String addContact(@RequestBody Contact contacts) {
+        for (Contact exitsContact : contact) {
+            if (exitsContact.getEmail().equals(contacts.getEmail())) {
+                return "email already exists";
+            }
+        }
+        contact.add(contacts);
+        return "contact added successfully "+contacts.getName()+ "\n"+ contacts.getEmail()+"\n"+contacts.getPhone();
+    }
+
+
+    @GetMapping("/contactDetails")
+    public String contactDetails(@RequestParam String name) {
+        for (Contact contact : contact) {
+            if (contact.getName().equals(name)) {
+                return "Name: " + contact.getName() + "\nEmail: " + contact.getEmail() + "\nphone: " + contact.getPhone();
+            }
+        }
+        return "contact not found";
+    }
 }
+
 
 
 
