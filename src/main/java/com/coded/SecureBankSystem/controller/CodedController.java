80b9d78f3a83ca9fe12785229d35b515a90465f4
Awdhah A N H S A Alazemi <83318@kfh.com>
2024-02-05 08:19:18 +0300

    both tasks

--- src/main/java/com/coded/SecureBankSystem/controller/CodedController.java
+++ src/main/java/com/coded/SecureBankSystem/controller/CodedController.java
@@ -4,11 +4,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class CodedController {
-    public ArrayList<Contact> contact = new ArrayList<>();
+    public List<Contact> contact = new ArrayList<>();
 
 
     @GetMapping("/sayHi")
